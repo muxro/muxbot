8a89ad3410f84054f6ad7f9272d1b2b1b8d591e2
CombineNConquer <alexvasiluta@protonmail.com>
2019-12-10 17:32:58 +0200

    Add .g and .gis

--- main.go
+++ main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"flag"
 	"fmt"
+	"net/http"
+	"net/url"
 	"os"
 	"os/signal"
 	"strings"
 	"syscall"
+
+	"github.com/PuerkitoBio/goquery"
 
 	"github.com/Knetic/govaluate"
 
@@ -67,25 +71,39 @@ func messageCreate(session *discordgo.Session, message *discordgo.MessageCreate)
 .ping - pong
 .echo - echoes back whatever you send it
 .calc - compute simple expression
-			`, session, message.ChannelID)
+			`, session, message.Message)
+		} else if strings.HasPrefix(message.Content, ".g ") {
+			res, err := ScrapeFirstWebRes(commandMessage)
+			if err != nil {
+				sendMessageInChannel(fmt.Sprintf("Error: %v", err), session, message.Message)
+				return
+			}
+			sendMessageInChannel(fmt.Sprintf("%s -- %s", res["url"], res["desc"]), session, message.Message)
+		} else if strings.HasPrefix(message.Content, ".gis ") {
+			res, err := ScrapeFirstImgRes(commandMessage)
+			if err != nil {
+				sendMessageInChannel(fmt.Sprintf("Error: %v", err), session, message.Message)
+				return
+			}
+			sendMessageInChannel(res, session, message.Message)
 		} else if strings.HasPrefix(message.Content, ".ping") {
-			sendMessageInChannel("pong", session, message.ChannelID)
+			sendMessageInChannel("pong", session, message.Message)
 		} else if strings.HasPrefix(message.Content, ".echo") {
-
 			if commandMessage != "" {
-				sendMessageInChannel(commandMessage, session, message.ChannelID)
+				sendMessageInChannel(commandMessage, session, message.Message)
 			}
 		} else if strings.HasPrefix(message.Content, ".calc") {
 			expr, err := govaluate.NewEvaluableExpression(commandMessage)
 			if err != nil {
-				sendMessageInChannel(fmt.Sprintf("Nu am putut calcula: Eroare %v", err), session, message.ChannelID)
+				sendMessageInChannel(fmt.Sprintf("Nu am putut calcula: Eroare %v", err), session, message.Message)
 				return
 			}
 			result, err := expr.Evaluate(nil)
 			if err != nil {
-				sendMessageInChannel(fmt.Sprintf("Nu am putut calcula, eroare %v", err), session, message.ChannelID)
+				sendMessageInChannel(fmt.Sprintf("Nu am putut calcula, eroare %v", err), session, message.Message)
+				return
 			}
-			sendMessageInChannel(fmt.Sprintf("%v", result), session, message.ChannelID)
+			sendMessageInChannel(fmt.Sprintf("%v", result), session, message.Message)
 		}
 	}
 
@@ -100,10 +118,62 @@ func sendEmbedInChannel(embed *discordgo.MessageEmbed, session *discordgo.Sessio
 
 }
 
-func sendMessageInChannel(message string, session *discordgo.Session, channel string) {
-	session.ChannelMessageSend(channel, message)
+func sendMessageInChannel(message string, session *discordgo.Session, originalMessage *discordgo.Message) {
+	session.ChannelMessageSend(originalMessage.ChannelID, fmt.Sprintf("(%s) %s", strings.Split(originalMessage.Author.String(), "#")[0], message))
 }
 
-func typeof(v interface{}) string {
-	return fmt.Sprintf("%T", v)
+func scrapeWeb(url string) (*goquery.Document, error) {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.133 Mobile Safari/535.19")
+
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	doc, err := goquery.NewDocumentFromResponse(res)
+	if err != nil {
+		return nil, err
+	}
+	return doc, nil
+}
+
+// ScrapeFirstWebRes function scrapes dogpile.com for the first result of the query and returns it
+func ScrapeFirstWebRes(q string) (map[string]string, error) {
+	url := "https://www.dogpile.com/serp?qc=web&q=" + url.QueryEscape(q)
+	doc, err := scrapeWeb(url)
+	if err != nil {
+		return nil, err
+	}
+
+	results := doc.Find(".layout__mainline").First()
+
+	sel := results.Find(".web-bing__result").First()
+
+	resURL := sel.Find(".web-bing__url").Text()
+	resDesc := sel.Find(".web-bing__description").Text()
+
+	return map[string]string{"url": resURL, "desc": resDesc}, nil
+}
+
+// ScrapeFirstImgRes function scrapes dogpile.com for the first result of the query and returns it
+func ScrapeFirstImgRes(q string) (string, error) {
+	url := "https://www.dogpile.com/serp?qc=images&q=" + url.QueryEscape(q)
+	doc, err := scrapeWeb(url)
+	if err != nil {
+		return "", err
+	}
+
+	results := doc.Find(".layout__mainline").First()
+
+	sel := results.Find(".image").First()
+	link := sel.Find("a").First()
+	url, _ = link.Attr("href")
+
+	return url, nil
 }
